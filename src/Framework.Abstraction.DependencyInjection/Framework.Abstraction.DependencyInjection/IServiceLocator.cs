754191aff4de67a61d940c2fb507f94b2b439a72
Alirexaa <Alireza.Baloochi1380@gmail.com>
2022-08-29 23:23:12 +0430

    Static Service Activator added

---
+++ src/Framework.Abstraction.DependencyInjection/Framework.Abstraction.DependencyInjection/IServiceLocator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.Abstraction.DependencyInjection;
+
+public interface IServiceLocator : IDisposable
+{
+    IServiceScope CreateScope();
+    T Get<T>();
+}
---
+++ src/Framework.DependencyInjection/ServiceActivator.cs
@@ -0,0 +1,29 @@
+﻿using Framework.Abstraction.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.DependencyInjection
+{
+    public static class ServiceActivator
+    {
+        private static IServiceProvider _serviceProvider;
+        private static IServiceScope _scope;
+        public static void Configure(IServiceProvider serviceProvider)
+        {
+            ArgumentNullException.ThrowIfNull(serviceProvider, nameof(serviceProvider));
+            _serviceProvider = serviceProvider;
+        }
+
+        public static IServiceScope CreateScope()
+        {
+            _scope = _serviceProvider.CreateScope();
+            return _scope;
+        }
+
+        public static T Get<T>()
+        {
+            CreateScope();
+
+            return _scope.ServiceProvider.GetService<T>();
+        }
+    }
+}
